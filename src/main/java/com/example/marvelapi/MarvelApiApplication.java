fc0720b6ebe75f32569d6bec7a981e8a4900738a
iarrabej <ivan.arrabejimenez.st@emeal.nttdata.com>
2022-10-13 10:41:36 +0200

    Init app

---
+++ src/main/java/com/example/marvelapi/MarvelApiApplication.java
@@ -0,0 +1,15 @@
+package com.example.marvelapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+public class MarvelApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MarvelApiApplication.class, args);
+        System.out.println("sadfsdf");
+    }
+
+}
